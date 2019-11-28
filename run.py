1be54df60567403137a9b8adce10137cec50b7f8
Vijay Kurian <kurianvijay7@gmail.com>
2019-11-28 10:55:50 +0000

    Add PORT and Procfile

--- run.py
+++ run.py
@@ -2,8 +2,10 @@ import os
 
 from src.app import create_app
 
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
+
 if __name__ == '__main__':
-  env_name = os.getenv('FLASK_ENV')
-  app = create_app(env_name)
+  port = os.getenv('PORT')
   # run app
-  app.run() 
+  app.run(host='https://localhost:5000', port=port)
